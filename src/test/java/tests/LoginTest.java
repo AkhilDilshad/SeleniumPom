9480fe13fdc487d653d20f308f9a0c18a84e8836
AkhilDilshad <akhildilshadt@gmail.com>
2025-07-05 00:08:18 +0530

    Log4jupdate

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -5,14 +5,17 @@ import org.testng.annotations.Test;
 
 import base.BaseTest;
 import pages.LoginPage;
+import utils.Log;
 
 public class LoginTest extends BaseTest {
 	@Test
 	public void validLogin() {
 		LoginPage loginpage = new LoginPage(driver); // Using the constructor from login page java class
+		Log.info("Entering Credentials");
 		loginpage.enterUser("admin@yourstore.com");
 		loginpage.enterPass("admin");
 		loginpage.login();
+		Log.info("Verifying page title");
 		Assert.assertEquals(driver.getTitle(), "Just a moment...");
 	}
 }
